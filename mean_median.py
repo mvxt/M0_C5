d91a3b8d70ea563de2a3fb2c086e79e8c4558d79
Michael Vinh Xuan Thanh <meta.orphic@gmail.com>
2020-07-20 17:12:28 -0600

    Complete challenge 5

--- mean_median.py
+++ mean_median.py
@@ -1,12 +1,37 @@
 # M0_C5 - Mean, Median
 
 def mean(scores):
-    # Write your code here
-    return "not implemented"
+    total = 0
+    count = 0
+    for score in scores:
+        # If score is valid
+        if score <= 100 and score >= 0:
+            total += score
+            count += 1
+
+    if count == 0:
+        return -1
+
+    return int(total / count)
 
 def median(scores):
-    # Write your code here
-    return "not implemented"
+    valid_scores = []
+    # Get separate list of valid scores
+    for score in scores:
+        # If score is valid
+        if score <= 100 and score >= 0:
+            valid_scores.append(score)
+
+    if len(valid_scores) == 0:
+        return -1
+
+    middle = len(valid_scores) // 2
+    if len(valid_scores) % 2 == 0:
+        lower = valid_scores[middle - 1]
+        upper = valid_scores[middle]
+        return int((lower + upper) / 2)
+
+    return valid_scores[middle]
 
 if __name__ == '__main__':
     scores = input("Input list of test scores, space-separated: ")
